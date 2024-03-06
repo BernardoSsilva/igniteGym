2de9e66376f4ed84fda59799675f00962709b723
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-03-06 11:50:46 -0300

    downloaded all assets and fixed dev dependencies

--- App.tsx
+++ App.tsx
@@ -1,20 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { useFonts } from "expo-font";
+import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
 export default function App() {
+  const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View>
+      {/* {fontsLoaded ? <Text>"Aoba"</Text> : <Text>Carregando</Text>} */}
+
+      <Text>Hello world</Text>
+
+      <StatusBar
+       
+        backgroundColor="transparent"
+        translucent
+      />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
