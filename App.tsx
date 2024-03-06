c98fe4e5fe7e54b848f85f64fec762546c93dafd
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-03-06 15:21:03 -0300

    fixed status bar style

--- App.tsx
+++ App.tsx
@@ -1,22 +1,15 @@
 import { LoadingIndicator } from "@components/loading/loading";
 import { Roboto_400Regular, Roboto_700Bold } from "@expo-google-fonts/roboto";
+import { SignUp } from "@screens/signup";
 import { useFonts } from "expo-font";
 import { StatusBar } from "expo-status-bar";
-import { NativeBaseProvider, useTheme } from "native-base";
-import { Text, View } from "react-native";
-import { THEME } from "./src/theme";
-import { SignIn } from "@screens/signin";
-import { SignUp } from "@screens/signup";
+import { NativeBaseProvider } from "native-base";
 
 export default function App() {
   const [fontsLoaded] = useFonts({ Roboto_400Regular, Roboto_700Bold });
   return (
     <NativeBaseProvider>
-      <StatusBar
-    
-        backgroundColor="transparent"
-        translucent
-      />
+      <StatusBar style="light" backgroundColor="transparent" translucent />
       {fontsLoaded ? <SignUp /> : <LoadingIndicator />}
     </NativeBaseProvider>
   );
