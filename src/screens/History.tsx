105c7ddc13443167114611c22c71cb204c92d447
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-03-07 16:13:33 -0300

    created empty content message

--- src/screens/History.tsx
+++ src/screens/History.tsx
@@ -1,12 +1,13 @@
 import { DefaultHeader } from "@components/defaultHeader/defaultHeader";
 import { HistoryCard } from "@components/historyCard/historyCard";
-import { Heading, SectionList, VStack } from "native-base";
+import { Heading, SectionList, VStack, Text } from "native-base";
 import { useState } from "react";
 
 export function History() {
   const [exercises, setExercises] = useState([
     {
       title: "26.08.22",
+
       data: [
         { title: "Costas", content: "puxada frontal", hour: "08:06" },
         { title: "Biceps", content: "Remada unilateral", hour: "08:06" },
@@ -36,6 +37,14 @@ export function History() {
           </Heading>
         )}
         px={8}
+        contentContainerStyle={
+          exercises.length === 0 && { flex: 1, justifyContent: "center" }
+        }
+        ListEmptyComponent={() => (
+          <Text color={"gray.100"} textAlign={"center"}>
+            Não ha exercícios concluídos ainda. {"\n"} Vamos treinar hoje?
+          </Text>
+        )}
       />
     </VStack>
   );
