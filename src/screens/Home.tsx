f36814e85756c4952a569f619b695edbbcc70cd0
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-03-07 13:36:54 -0300

    created filter list component and exercices header

--- src/screens/Home.tsx
+++ src/screens/Home.tsx
@@ -1,9 +1,16 @@
 import { FilterGroup } from "@components/filterGroup/filterGroup";
 import { HomeHeader } from "@components/homeHeader/homeHeader";
-import { HStack, VStack } from "native-base";
+import { FlatList, HStack, Heading, Text, VStack } from "native-base";
 import { useEffect, useState } from "react";
 
 export function Home() {
+  const [groups, setGroups] = useState([
+    "costa",
+    "peito",
+    "biceps",
+    "triceps",
+    "pernas",
+  ]);
   const [selectedGroup, setSelectedGroup] = useState("");
 
   useEffect(() => {
@@ -13,18 +20,35 @@ export function Home() {
   return (
     <VStack flex={1}>
       <HomeHeader />
-      <HStack>
-        <FilterGroup
-          name={"costas"}
-          isActive={selectedGroup === "costas"}
-          onPress={() => setSelectedGroup("costas")}
-        />
-        <FilterGroup
-          name={"peito"}
-          isActive={selectedGroup === "peito"}
-          onPress={() => setSelectedGroup("peito")}
-        />
-      </HStack>
+
+      <FlatList
+        data={groups}
+        renderItem={({ item }) => {
+          return (
+            <FilterGroup
+              name={item}
+              isActive={selectedGroup === item}
+              onPress={() => setSelectedGroup(item)}
+            />
+          );
+        }}
+        horizontal
+        showsHorizontalScrollIndicator={false}
+        _contentContainerStyle={{ px: 8 }}
+        my={10}
+        maxH={10}
+      />
+
+      <VStack flex={1} px={8}>
+        <HStack justifyContent={"space-between"} marginBottom={5}>
+          <Heading color={"gray.200"} fontSize={"md"}>
+            Exercícios
+          </Heading>
+          <Text color={"gray.200"} fontSize={"sm"}>
+            4
+          </Text>
+        </HStack>
+      </VStack>
     </VStack>
   );
 }
