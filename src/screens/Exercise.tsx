3e6f18b0bdd15f6dc8678953a38d6483a3ba8e28
BernardoSsilva <bernardo.ssilva0808@gmail.com>
2024-03-07 21:05:22 -0300

    created exercises info image

--- src/screens/Exercise.tsx
+++ src/screens/Exercise.tsx
@@ -1,4 +1,5 @@
 import { DefaultHeader } from "@components/defaultHeader/defaultHeader";
+import { ExerciseImage } from "@components/exerciseImage/exerciseImage";
 import { ExercisesHeader } from "@components/exercisesHeader/exercisesHeader";
 import { Center, Text, VStack } from "native-base";
 
@@ -6,6 +7,19 @@ export function Exercise() {
   return (
     <VStack flex={1}>
       <ExercisesHeader text={"Puxada frontal"} type={"Costas"} />
+      <Center
+        flex={1}
+        alignItems="center"
+        justifyContent={"top"}
+        padding={0}
+        bg={"red.100"}
+      >
+        <ExerciseImage
+          imageUri={
+            "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS55IEkn5f8Ud6SH_8GOMy0JPj43QiMXLPzJTmNf7O2Ow&s"
+          }
+        />
+      </Center>
     </VStack>
   );
 }
